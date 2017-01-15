51f3e1c48ce7341380d6d7530219fcbfcfb1ba66
dilan <hiradilan@gmail.com>
2017-01-15 13:01:54 -0600

    Forgot Password working with default template

--- manage.py
+++ manage.py
@@ -7,10 +7,10 @@ manager = Manager(app)
 @manager.command
 def initdb():
     db.create_all()
-    db.session.add(User(firstName="LeBron", lastName="James",email="dilan@webitup.com", password="test", stocks=[Tickers(ticker='ATVI', startingPrice=28.12)]))
+    db.session.add(User(firstName="LeBron", lastName="James",email="dilan@webitup.com", password="test",active=True, stocks=[Tickers(ticker='ATVI', startingPrice=28.12)]))
     apple = Tickers(ticker="AAPL", startingPrice=89.8)
     signet = Tickers(ticker="SIG", startingPrice=120)
-    db.session.add(User(firstName="Dilan", lastName="Hira",email="dilan@utexas.edu", password="test", stocks=[apple, signet]))
+    db.session.add(User(firstName="Dilan", lastName="Hira",email="dilan@utexas.edu", password="test", active=True, stocks=[apple, signet]))
     #db.session.add(apple)
     db.session.commit()
     print 'Initialized the database'
